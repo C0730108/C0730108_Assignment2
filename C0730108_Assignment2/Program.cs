7d1821ae3b39caff2cbda0c19e2c0da86f5503f0
jashandeepkaurkingra <jashandeepkaur717@gmail.com>
2019-03-07 13:57:57 -0500

    commit

--- C0730108_Assignment2/Program.cs
+++ C0730108_Assignment2/Program.cs
@@ -10,6 +10,28 @@ namespace C0730108_Assignment2
     {
         static void Main(string[] args)
         {
+            //Student Name Jashandeep kaur kingra(co730108)
+            //student Name Ramandeep kaur(c0722414)
+            //Assignment2
+            //March 7,2019
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+            
+        }
+    }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();    
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
